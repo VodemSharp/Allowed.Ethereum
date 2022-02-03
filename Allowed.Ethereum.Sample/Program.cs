88fd6d8f043be86e8923a574c9ace2f9c508b10c
VodemSharp <vadumfedor@gmail.com>
2022-02-03 16:06:06 +0200

    Add sample.

---
+++ Allowed.Ethereum.Sample/Program.cs
@@ -0,0 +1,8 @@
+﻿using Allowed.Ethereum.Common.Helpers;
+using Allowed.Ethereum.Common.Models;
+using Newtonsoft.Json;
+
+string source = @"c:/Users/VodemSharp/Desktop/Projects/Work/DARS/DARS.Contracts/Source/";
+string contractJson = File.ReadAllText(@$"{source}/bin/BEP20USDT.json");
+StandardInput usdtInput = StandardInputHelper.GetStandardInput(source, contractJson);
+File.WriteAllText("BEP20USDT-input.json", JsonConvert.SerializeObject(usdtInput, Formatting.Indented));
